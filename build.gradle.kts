46b88f01866bb0ba1c267203f4e74d5ef3c66a35
poelly <poelly@b2bcni.co.,kr>
2025-03-21 13:45:45 +0900

    부트스트랩 cdn 작성 및 페이지 구분

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,17 +24,30 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	// Spring Boot Starters
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// Thymeleaf Extras
+//	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.3.0")
+
+	// Lombok
 	annotationProcessor("org.projectlombok:lombok")
+	compileOnly("org.projectlombok:lombok")
+
+	// Database
+//	runtimeOnly("com.mysql:mysql-connector-j")
+
+	  // Test Dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+//	testImplementation("org.springframework.security:spring-security-test")
+
+	implementation("org.webjars:bootstrap:5.3.0")
+    implementation("org.webjars:jquery:3.6.0")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/b2bcni/b2bcni_webPproject/controller/IndexController.java
+++ src/main/java/com/b2bcni/b2bcni_webPproject/controller/IndexController.java
@@ -1,4 +1,12 @@
 package com.b2bcni.b2bcni_webPproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+     @GetMapping("/")
+    public String home() {
+        return "index";  // 타임리프 템플릿을 반환
+    }
 }
